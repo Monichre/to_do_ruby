150ceb9933936771adfba937c75f9884f120e1a1
Liam Ellis <liamhellis@gmail.com>
2016-07-13 16:50:03 -0700

    to do app}

--- lib/list.rb
+++ lib/list.rb
@@ -17,6 +17,16 @@ class List
     lists
   end
 
+  define_singleton_method(:find) do |id|
+    found_list = nil
+    List.all().each() do |list|
+      if list.id() == id
+        found_list = list
+      end
+    end
+    found_list
+  end
+
   def save
     result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
     @id = result.first().fetch("id").to_i
@@ -24,5 +34,16 @@ class List
 
   def ==(another_list)
     self.name() == (another_list.name()) && self.id() == (another_list.id())
+  end
+
+  def tasks
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{self.id()};")
+    tasks.each() do |task|
+      description = task.fetch('description')
+      list_id = task.fetch('list_id').to_i()
+      list_tasks.push(Task.new({:description => description, :list_id => list_id}))
+    end
+    list_tasks
   end
 end
